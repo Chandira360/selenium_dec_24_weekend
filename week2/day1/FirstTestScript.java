5ec51d73ae894c99a68f89f2b230edb8c60206ea
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-12-14 13:19:18 +0530

    week2_day1
    Selenium(introduction, architecture, sample program), basic html

---
+++ week2/day1/FirstTestScript.java
@@ -0,0 +1,34 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestScript {
+
+	public static void main(String[] args) {
+
+		// Launch the browser
+		ChromeDriver driver = new ChromeDriver();
+		// maximize the browser
+		driver.manage().window().maximize();
+		// load the application url
+		driver.get("http://leaftaps.com/opentaps/");
+		// locate the username field
+		driver.findElement(By.id(null));
+		WebElement usernameField = driver.findElement(By.id("username"));
+		// enter the input as demosalesmanager for usernameField
+		usernameField.sendKeys("demosalesmanager");
+		// locate the password field and enter the password as crmsfa
+		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+		//locate the login button and click
+		driver.findElement(By.className("decorativeSubmit")).click();
+		//to get the title of the webpage -> getTitle() and returnType as string
+		String title = driver.getTitle();
+		System.out.println("Title is "+title);
+		//locate the CRM/SFA link and click
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		System.out.println(driver.getTitle());
+	}
+
+}
