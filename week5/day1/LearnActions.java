25ae0fbeeda313e57cccb90241a49c67bee80d79
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-01-04 13:04:04 +0530

    week5_day1

---
+++ week5/day1/LearnActions.java
@@ -0,0 +1,23 @@
+package week5.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class LearnActions {
+
+	
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://myntra.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement men = driver.findElement(By.linkText("Men"));
+		Actions action = new Actions(driver);
+		action.moveToElement(men).build();
+		
+	}
+}
