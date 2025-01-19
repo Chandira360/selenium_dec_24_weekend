5a53bd1a71b422e52421115dbc8d3f7282cb6d16
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-01-19 13:53:58 +0530

    week7_day2

---
+++ week7/day2/runner/CucumberRunner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import stepDefinition.Base;
+//features = {"src/test/java/features/Login.feature","src/test/java/features/CreateLead.feature"},
+
+@CucumberOptions(features = {"src/test/java/features"},
+				 glue = {"stepDefinition"},
+				 publish = true,
+				 monochrome = true,
+				 tags = "@login")
+public class CucumberRunner extends Base {
+
+	
+}
